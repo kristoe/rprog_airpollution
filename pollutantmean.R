95c6f2b063c9d7f823cb90cf961b399379874719
Eric Kristoff <kristoe@yahoo.com>
2014-05-18 03:06:39 -0500

    part 1 looping now works

--- pollutantmean.R
+++ pollutantmean.R
@@ -12,35 +12,37 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
   ## Return the mean of the pollutant across all monitors list
   ## in the 'id' vector (ignoring NA values)
   
+  fooGood <- vector()
   
-  fileName <- id
+  for(i in id)
+  {
+    fileName <- i
   
-  if(id<100) {
-    fileName <- paste("0",id, sep="")
-  }
-  
-  if (id<10) {
-    fileName <- paste("00",id, sep="")
-  }
+    if(i<100) {
+      fileName <- paste("0",i, sep="")
+    }
+    if (i<10) {
+      fileName <- paste("00",i, sep="")
+    }
     
-  #print (fileName)
-  dataFile = read.table(paste(directory,"/",fileName,".csv", sep=""),sep=",",header=TRUE)
-  
-  if(pollutant == "sulfate") {
-    x <- dataFile[,2,drop=FALSE]
-  }
-  else if (pollutant == "nitrate") {
-    x <- dataFile[,3,drop=FALSE]
     
-  } else {
-    stop( "Invalid pollutant")
+    dataFile = read.table(paste(directory,"/",fileName,".csv", sep=""),sep=",",header=TRUE)
+  
+    if(pollutant == "sulfate") {rm
+     x <- dataFile[,2,drop=FALSE]
+    }
+    else if (pollutant == "nitrate") {
+      x <- dataFile[,3,drop=FALSE]
+    } else {
+     stop( "Invalid pollutant")
+    }
+  
+   bad = is.na(x)
+   good = x[!bad]
     
+   fooGood <- c(good,fooGood)
   }
-  
-  bad = is.na(x)
-  good = x[!bad]
-  
-  round(mean(good),digits=3)
+  round(mean(fooGood),digits=3)
 }
 
 setwd("/users/Eric/My_Dev/coursera/rprog/rprog_airpollution")
